cfa0f853d755482f349be9998d334cd728cf3a1c
Plinio Balduino <pbalduino+github@gmail.com>
2013-02-22 20:12:08 -0300

    color

--- capitulo_13/javascripts/validador.js
+++ capitulo_13/javascripts/validador.js
@@ -1,8 +1,26 @@
 ;(function($) {
   $.fn.validador = function() {
     var init = function() {
+
       var element = $(this);
-      console.log(element.attr("id"));
+        
+    	function isValid(text) {
+    		return (text === "");
+    	}
+
+      function onElementBlur() {
+        var texto = element.val();
+
+        if(isValid(texto)) {
+          element.removeClass("erro");
+          element.addClass("valido");
+        } else {
+          element.removeClass("valido");
+          element.addClass("erro");
+        }
+      }
+
+      element.bind("blur.validador", onElementBlur);
     }
 
     return this.each(init);
