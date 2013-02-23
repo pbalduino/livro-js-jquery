ed8b036f30dd77845ffda9910d862f12250115be
Plinio Balduino <pbalduino+github@gmail.com>
2013-02-23 19:48:17 -0300

    Validando CPF

--- capitulo_13/javascripts/validador.js
+++ capitulo_13/javascripts/validador.js
@@ -3,9 +3,58 @@
     var init = function() {
 
       var element = $(this);
+
+      function validaCpf(text) {
+        text = text.replace(/\D/g, "");
+
+        if(text.length === 11) {
+          var digitos = [];
+
+          for(var pos = 0; pos < text.length; pos++) {
+            digitos[pos] = text.charCodeAt(pos) - 48;
+          }
+
+          // calcular o primeiro digito
+          var primeiroPasso = [];
+
+          var soma = 0;
+
+          for(var pos = 0; pos < 9; pos++) {
+            soma += digitos[pos] * (10 - pos);
+          }
+
+          var primeiroDigito = 11 - (soma % 11);
+
+          if(primeiroDigito > 9) {
+            primeiroDigito = 0;
+          }
+
+          if(digitos[9] !== primeiroDigito) {
+            return false;
+          }
+
+          // calcular o segundo digito
+
+          soma = 0;
+
+          for(var pos = 0; pos < 10; pos++) {
+            soma += digitos[pos] * (11 - pos);
+          }
+
+          var segundoDigito = 11 - (soma % 11);
+
+          if(segundoDigito > 9) {
+            segundoDigito = 0;
+          }
+
+          return digitos[10] === segundoDigito;
+        }
+
+        return false;
+      }
         
     	function isValid(text) {
-    		return (text === "");
+    		return validaCpf(text);
     	}
 
       function onElementBlur() {
